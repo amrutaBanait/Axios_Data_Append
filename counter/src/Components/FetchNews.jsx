ac052190ad29e0380f1c7e3637459259f1a0aad5
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-26 20:57:18 +0530

    cmt

--- counter/src/Components/FetchNews.jsx
+++ counter/src/Components/FetchNews.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 const FetchNews = () => {
 
+const [news, setNews] = useState([]);
+
+
 const fetchNews = () => {
-  console.log("clicked")
+  axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=292c45a19778404eaa8b2dfb34a370f1")
+  .then((response) => {
+
+    setNews (response.data.articles)
+  })
 }
 
   return (
@@ -16,6 +23,10 @@ const fetchNews = () => {
           </div>
         </div>
       </div>
+
+      <div className="container">
+        
+        </div>
     </>
   );
 };
