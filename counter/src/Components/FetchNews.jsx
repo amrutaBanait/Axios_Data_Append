ffcf4b4a8369cde121e12b406c12afe566862467
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-26 21:03:17 +0530

    cmt

--- counter/src/Components/FetchNews.jsx
+++ counter/src/Components/FetchNews.jsx
@@ -1,32 +1,50 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const FetchNews = () => {
+  const [news, setNews] = useState([]);
 
-const [news, setNews] = useState([]);
-
-
-const fetchNews = () => {
-  axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=292c45a19778404eaa8b2dfb34a370f1")
-  .then((response) => {
-
-    setNews (response.data.articles)
-  })
-}
+  const fetchNews = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/top-headlines?country=in&apiKey=292c45a19778404eaa8b2dfb34a370f1"
+      )
+      .then((response) => {
+        setNews(response.data.articles);
+      });
+  };
 
   return (
     <>
       <div className="container my-3">
         <div className="row">
           <div className="col-4">
-            <button className="btn btn-primary" onClick={fetchNews}>FetchNews</button>
+            <button className="btn btn-primary" onClick={fetchNews}>
+              FetchNews
+            </button>
           </div>
         </div>
       </div>
 
       <div className="container">
-        
+        <div className="row">
+          <div className="col-4">
+            <div className="card" style={{width: "18rem"}}>
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   );
 };
